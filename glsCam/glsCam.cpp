5daef6b546906346f62f83e1ff4ff4b784ad2db3
oasi-adamay <oasi.adamay@gmail.com>
2016-05-08 03:00:42 +0900

    glsCam FFT rect表示

--- glsCam/glsCam.cpp
+++ glsCam/glsCam.cpp
@@ -18,13 +18,15 @@ enum E_CAM_MODE {
 	NORMAL,
 	GRAY,
 	FFT,
+	FFT_RECT,
 };
 
 void controls(GLFWwindow* window, int& mode){
 
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) mode = E_CAM_MODE::FFT;
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) mode = E_CAM_MODE::FFT_RECT;
+	if (glfwGetKey(window, GLFW_KEY_9) == GLFW_PRESS) mode = E_CAM_MODE::GRAY;
 	if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS) mode = E_CAM_MODE::NORMAL;
-	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) mode = E_CAM_MODE::GRAY;
-	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) mode = E_CAM_MODE::FFT;
 }
 
 
@@ -58,6 +60,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mat fftwin;
 	cv::createHanningWindow(fftwin, sizeFft, CV_32F);
 	glsMat glsFftWin(fftwin);
+	glsMat glsZero(Mat::zeros(sizeFft, CV_32FC1));
+
+	Rect rectFft((captureWidth - sizeFft.width) / 2, (captureHeight - sizeFft.height) / 2 , sizeFft.width, sizeFft.height);
 
 
 	int camMode = E_CAM_MODE::FFT;
@@ -68,73 +73,61 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		cv::flip(frame, frame, 0);				// è„â∫îΩì]
 
-		glsMat glsFrame(frame);
-		glsConvert(glsFrame, glsFrame, 1.0f / 256.0f);
-		switch (camMode){
-		case(E_CAM_MODE::FFT) :
-			{
+		glsMat glsFrame;
 
-				int x = (glsFrame.size().width - sizeFft.width) / 2;
-				int y = (glsFrame.size().height - sizeFft.height) / 2;
-				Rect rect( x, y, sizeFft.width, sizeFft.height);
+		switch (camMode){
+		case(E_CAM_MODE::FFT) :	{
 #if 0
-				Mat roi = Mat(frame, rect).clone();
-				cv::cvtColor(roi,roi,CV_BGR2GRAY);
-				roi.convertTo(roi, CV_32FC1,1.0/256.0);
-				Mat zero = Mat::zeros(roi.size(),roi.type());
-				vector<Mat> pln(2);
-				pln[0] = roi;
-				pln[1] = zero;
-				Mat img;
-				cv::merge(pln, img);
-				cv::dft(img, img);
-				cv::split(img, pln);
-				Mat mag;
-				cv::magnitude(pln[0], pln[1], mag);
-				cv::log(mag+1,mag);
-				cv::normalize(mag, mag, 0, 1, CV_MINMAX);
-				cv::imshow("[CV MAG]",mag);
-#elif 0
-				glsMat glsZero(sizeFft, CV_32FC1,Size(2,2));
-				glsCopyRect(glsFrame, glsFrame, rect , Size(2,2));
-				glsCvtColor(glsFrame, glsFrame, CV_BGR2GRAY);
-				glsMultiply(glsFftWin, glsFrame, glsFrame);
-				vector<glsMat> plnGls(2);
-				plnGls[0] = glsFrame;
-				plnGls[1] = glsZero;
-				glsMat glsComplx;
-				glsMerge(plnGls, glsComplx);
-				glsFft(glsComplx, glsComplx, GLS_FFT_SHIFT);
-				glsMagSpectrums(glsComplx, glsFrame);
-				glsAdd(vec4(1.0), glsFrame, glsFrame);
-				glsLog(glsFrame, glsFrame);
-				double min, max;
-				glsMinMaxLoc(glsFrame, &min, &max);
-				glsAdd(vec4((float)-min), glsFrame, glsFrame);
-				glsMultiply(vec4(1.0f / (float)(max - min)), glsFrame, glsFrame);
+			Mat roi = Mat(frame, rect).clone();
+			cv::cvtColor(roi,roi,CV_BGR2GRAY);
+			roi.convertTo(roi, CV_32FC1,1.0/256.0);
+			Mat zero = Mat::zeros(roi.size(),roi.type());
+			vector<Mat> pln(2);
+			pln[0] = roi;
+			pln[1] = zero;
+			Mat img;
+			cv::merge(pln, img);
+			cv::dft(img, img);
+			cv::split(img, pln);
+			Mat mag;
+			cv::magnitude(pln[0], pln[1], mag);
+			cv::log(mag+1,mag);
+			cv::normalize(mag, mag, 0, 1, CV_MINMAX);
+			cv::imshow("[CV MAG]",mag);
 #else
-				glsMat glsZero(sizeFft, CV_32FC1, Size(2, 2));
-				glsCopyRect(glsFrame, glsFrame, rect, Size(2, 2));
-				glsCvtColor(glsFrame, glsFrame, CV_BGR2GRAY);
-				glsMultiply(glsFftWin, glsFrame, glsFrame);
-				vector<glsMat> plnGls(2);
-				plnGls[0] = glsFrame;
-				plnGls[1] = glsZero;
-				glsMat glsComplx;
-				glsMerge(plnGls, glsComplx);
-				glsFft(glsComplx, glsComplx, GLS_FFT_SHIFT);
-				glsLogMagSpectrums(glsComplx, glsFrame, 1.0);
-				glsNormalize(glsFrame, glsFrame, 0, 1, NORM_MINMAX);
-#endif
-			}
-			break;
-		case(E_CAM_MODE::GRAY) :
+			Mat roi = Mat(frame, rectFft);
+			glsFrame = roi;
+			glsConvert(glsFrame, glsFrame, 1.0f / 256.0f);
 			glsCvtColor(glsFrame, glsFrame, CV_BGR2GRAY);
-			break;
+			glsMultiply(glsFftWin, glsFrame, glsFrame);
+			vector<glsMat> plnGls(2);
+			plnGls[0] = glsFrame;
+			plnGls[1] = glsZero;
+			glsMat glsComplx;
+			glsMerge(plnGls, glsComplx);
+			glsFft(glsComplx, glsComplx, GLS_FFT_SHIFT);
+			glsLogMagSpectrums(glsComplx, glsFrame, 1.0);
+			glsNormalize(glsFrame, glsFrame, 0, 1, NORM_MINMAX);
+#endif
+		}break;
+		case(E_CAM_MODE::FFT_RECT) : {
+			Mat roi = Mat(frame, rectFft);
+			glsFrame = roi;
+			glsConvert(glsFrame, glsFrame, 1.0f / 256.0f);
+			glsCvtColor(glsFrame, glsFrame, CV_BGR2GRAY);
+			glsMultiply(glsFftWin, glsFrame, glsFrame);
+		} break;
+		case(E_CAM_MODE::GRAY) : {
+			glsFrame = frame;
+			glsConvert(glsFrame, glsFrame, 1.0f / 256.0f);
+			glsCvtColor(glsFrame, glsFrame, CV_BGR2GRAY);
+		}break;
 		case(E_CAM_MODE::NORMAL):
-		default:
+		default:{
+			glsFrame = frame;
+			glsConvert(glsFrame, glsFrame, 1.0f / 256.0f);
 			glsCvtColor(glsFrame, glsFrame, CV_BGR2RGB);
-			break;
+		}break;
 		}
 
 		glfwSetWindowSize(window, glsFrame.size().width, glsFrame.size().height);
