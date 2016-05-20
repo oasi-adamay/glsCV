47b160462dc43784396d3b2016590226efba8a1c
oasi-adamay <oasi.adamay@gmail.com>
2016-05-20 18:44:00 +0900

    Closed #23

--- glsCV/glsShader.h
+++ glsCV/glsShader.h
@@ -31,13 +31,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef _GLS_SHADER_H_
 #define _GLS_SHADER_H_
 
-#include "glsVAO.h"
-#include "glsFBO.h"
-
-#ifdef _DEBUG
-//#define _DEBUG_SHADER
-#endif
-
 namespace gls
 {
 
@@ -60,8 +53,6 @@ private:
 
 	/*!
 	setup info 
-
-	setup関数内で更新される。
 	*/
 	struct  SetupInfo{
 		int argnum;			//Execute()の引数の数
@@ -70,125 +61,15 @@ private:
 		Size texDstSize;	//dst textureのsize
 	};
 
-	// param が1個のために必要
-	void setup(SetupInfo* info) //const
-	{
-		const int i = info->argnum++;
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "end!!" << std::endl;
-#endif
-	}
-
-	template<class T>
-	void setup(SetupInfo* info, const T& t)
-	{
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "T:" << t << std::endl;
-#endif
-	}
-
-	template<>
-	void setup<int>(SetupInfo* info, const int& t)
-	{
-		const int i = info->argnum++;
-		GLuint loc = uniformLocArray[i];
-		glUniform1i(loc, t);
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "loc:" << loc << "\t";
-		std::cout << "val:" << t << endl;
-#endif
-	}
-
-	template<>
-	void setup<float>(SetupInfo* info, const float& t)
-	{
-		const int i = info->argnum++;
-		GLuint loc = uniformLocArray[i];
-		glUniform1f(loc, t);
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "loc:" << loc << "\t";
-		std::cout << "val:" << t << endl;
-#endif
-	}
-
-	template<>
-	void setup<double>(SetupInfo* info, const double& t)
-	{
-		const int i = info->argnum++;
-		GLuint loc = uniformLocArray[i];
-		float val = (float)t;
-		glUniform1f(loc, val);
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "loc:" << loc << "\t";
-		std::cout << "val:" << t << endl;
-#endif
-	}
-
-	template<>
-	void setup<Scalar>(SetupInfo* info, const Scalar& t)
-	{
-		const int i = info->argnum++;
-		GLuint loc = uniformLocArray[i];
-		float val[4] = { (float)t[0], (float)t[1], (float)t[2], (float)t[3] };
-		glUniform4fv(loc, 1, val);
-#ifdef _DEBUG_SHADER
-		std::cout << "i:" << i << "\t";
-		std::cout << "loc:" << loc << "\t";
-		std::cout << "val:" << t << endl;
-#endif
-	}
-
-
-	template<>
-	void setup<GlsMat>(SetupInfo* info, const GlsMat& t)
-	{
-		const int i = info->argnum++;
-		if (i<uniformLocArray.size()){	//src texture
-			if (!t.empty()){
-				GLuint loc = uniformLocArray[i];
-				int id = info->texSrcNum++;
-				glActiveTexture(GL_TEXTURE0 + id);
-				glBindTexture(GL_TEXTURE_2D, t.texid());
-				glUniform1i(loc, id);
-				
-#ifdef _DEBUG_SHADER
-				std::cout << "i:" << i << "\t";
-				std::cout << "loc:" << loc << "\t";
-				std::cout << "texunit:" << id << "\t";
-				std::cout << "texSrc:" << t.texid() << endl;
-#endif
-			}
-
-		}
-		else{	//dst texture
-			if (!t.empty()){
-				int id = info->texDstNum++;
-				info->texDstSize = t.size();
-				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + id, GL_TEXTURE_2D, t.texid(), 0);
-				//GLS_Assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
-#ifdef _DEBUG_SHADER
-				std::cout << "i:" << i << "\t";
-				std::cout << "attach:" << id << "\t";
-				std::cout << "texdst:" << t.texid() << endl;
-#endif
-			}
-
-		}
-	}
-
+	//! setup (uniform / bind / ...)
+	template<class T>	void setup(SetupInfo* info, const T& t);
+	template<>	void setup<int>(SetupInfo* info, const int& t);
+	template<>	void setup<float>(SetupInfo* info, const float& t);
+	template<>	void setup<double>(SetupInfo* info, const double& t);
+	template<>	void setup<Scalar>(SetupInfo* info, const Scalar& t);
+	template<>	void setup<GlsMat>(SetupInfo* info, const GlsMat& t);
 	template<class First, class... Rest>
-	void setup(SetupInfo* info, const First& first, const Rest&... rest)
-	{
-		setup(info, first);
-		setup(info, rest...);
-	}
-
-	void DrawBuffers(const int attachment_num);
+	void setup(SetupInfo* info, const First& first, const Rest&... rest);
 
 
 protected:
@@ -256,13 +137,22 @@ public:
 
 	void InitAll(void);
 
+	/*!
+	Shaderの実行
+	*/
+	template<class First, class... Rest>
+	void Execute(const First& first, const Rest&... rest);
+
 
 	/*!
-	uniform設定
-
+	OpenGL API wrap
 	*/
 
-	void setUniform(const string& name , const int & val) const{
+	//! glDrawBuffers wrap
+	void DrawBuffers(const int attachment_num);
+
+	//! uniform設定
+	void setUniform(const string& name, const int & val) const{
 		glUniform1i(glGetUniformLocation(program(),name.c_str()), val);
 	}
 
@@ -276,53 +166,7 @@ public:
 	}
 
 
-	template<class First, class... Rest>
-	void Execute(const First& first, const Rest&... rest);
-
-
 };
-
-
-template<class First, class... Rest>
-void glsShaderBase::Execute(const First& first, const Rest&... rest)
-{
-	glsVAO vao(glGetAttribLocation(program(), "position"));
-	glsFBO fbo(1);
-
-	//program
-	{
-		glUseProgram(program());
-	}
-
-	//setup
-	SetupInfo info = { 0 };
-
-	{
-		setup(&info, first);
-		setup(&info, rest...);
-	}
-#ifdef _DEBUG_SHADER
-	cout << "argnum:" << info.argnum << endl;
-	cout << "texSrcNum:" << info.texSrcNum << endl;
-	cout << "texDstNum:" << info.texDstNum << endl;
-	cout << "texDstSize:" << info.texDstSize << endl;
-
-#endif
-
-	DrawBuffers(info.texDstNum);
-
-	//Viewport
-	glViewport(0, 0, info.texDstSize.width, info.texDstSize.height);
-
-	//Render!!
-	{
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-		glFlush();
-	}
-
-//	GL_CHECK_ERROR();
-
-}
 
 
 
@@ -331,10 +175,13 @@ Initialize all shader
 */
 void ShaderInitAll(void);
 
-
-
-
 }//namespace gls
+
+
+/*
+Template include
+*/
+#include "glsShader_impl.h"
 
 
 #endif
